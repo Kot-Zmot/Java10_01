fe9a6dd3696bc2a9e07738c091f9ddfaedc16a69
Kot-Zmot <meledin.av52@gmail.com>
2022-12-10 17:54:08 +0300

    lombok

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,5 +1,13 @@
 package ru.netology;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+
 public class Radio {
     private int currentVolume;
     private int currentStation;
@@ -8,81 +16,27 @@ public class Radio {
     private int minVolume;
     private int maxVolume;
 
-    //Конструкторы
-    public Radio() { //значения по умолчанию
-        minStation = 0;
-        maxStation = 9;
-        minVolume = 0;
-        maxVolume = 10;
+    public Radio() {
     }
 
-    public Radio(int sizeRadio) { //поле значения кол-ва станций
-        maxStation = sizeRadio - 1;
+    public Radio(int maxStation) {
+        this.maxStation = maxStation;
     }
 
-    //Геттеры
     public int getCurrentVolume() {
         return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        this.currentVolume = currentVolume;
     }
 
     public int getCurrentStation() {
         return currentStation;
     }
 
-    //Сеттер громкости
-    public void setCurrentVolume(int newCurrentVolume) {
-        if (newCurrentVolume < minVolume) {
-            return;
-        }
-        if (newCurrentVolume > maxVolume) {
-            return;
-        }
-        currentVolume = newCurrentVolume;
+    public void setCurrentStation(int currentStation) {
+        this.currentStation = currentStation;
     }
-
-    // Сеттер радиостанций
-    public void setCurrentStation(int newCurrentStation) {
-        if (newCurrentStation < minStation) {
-            return;
-        }
-        if (newCurrentStation > maxStation) {
-            return;
-        }
-        currentStation = newCurrentStation;
-    }
-
-    //Увеличение громкости на +1
-    public void increaseVolume() {
-        if (getCurrentVolume() < maxVolume) {
-            setCurrentVolume(currentVolume + 1);
-        }
-    }
-
-    //Понижение громкости на -1
-    public void decreaseVolume() {
-        if (getCurrentVolume() > minVolume) {
-            setCurrentVolume(currentVolume - 1);
-        }
-    }
-
-    //Переключение радиостанции на +1
-    public void nextStation() {
-        if (getCurrentStation() < maxStation) {
-            setCurrentStation(currentStation + 1);
-        } else {
-            setCurrentStation(minStation);
-        }
-    }
-
-    //Переключение радиостанции на -1
-    public void prevStation() {
-        if (getCurrentStation() > minStation) {
-            setCurrentStation(currentStation - 1);
-        } else {
-            setCurrentStation(maxStation);
-        }
-    }
-
-
 }
 
